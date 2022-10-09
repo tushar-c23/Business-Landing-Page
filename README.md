20c7c52383ac41c90c355b122385897949b99a58
tusharc <tusharc.cse@gmail.com>
2022-10-10 00:14:38 +0530

    Updates

---
+++ README.md
@@ -0,0 +1,14 @@
+# Business-Landing-Page
+A business landing page made with HTML CSS and Bootstrap [Build to Learn]
+
+## Progress Record
+Go to [Progress](./progress.md).
+
+## Learnings
+1. Using several types of classes both custom defined and bootstrap's predefined classes to manipulate several div(s).
+2. Using and understanding several CSS properties
+   - Media Queries
+   - Flexbox
+   - Bootstrap properties
+     - xl md and other size configurations
+3. JavaScript and DOM
---
+++ js/main.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+
+  // STICKY HEADER ON SCROLL
+
+  const selectHeader = document.querySelector('#header');
+  if(selectHeader) {
+    document.addEventListener('scroll', () => {
+      window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
+    });
+  }
+
+  // STICKY HEADER ON SCROLL
+
+  // MOBILE NAV TOGGLE
+  
+  const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
+  if(mobileNavToggle) {
+    mobileNavToggle.addEventListener('click',function(event) {
+      event.preventDefault();
+  
+      document.querySelector('body').classList.toggle('mobile-nav-active');
+  
+      this.classList.toggle('bi-list');
+      this.classList.toggle('bi-x');
+    });
+  }
+  
+  // MOBILE NAV TOGGLE
+  
+  // TOGGLE MOBILE NAV DROPDOWNS
+  
+  const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+  
+  navDropdowns.forEach(el => {
+    el.addEventListener('click', function(event) {
+      if(document.querySelector('.mobile-nav-active')) {
+        event.preventDefault();
+        this.classList.toggle('active');
+        this.nextElementSibling.classList.toggle('dropdown-active');
+  
+        let dropDrownIndicator = this.querySelector('.dropdown-indicator');
+        dropDrownIndicator.classList.toggle('bi-chevron-up');
+        dropDrownIndicator.classList.toggle('bi-chevron-down');
+      }
+    })
+  });
+  
+  // TOGGLE MOBILE NAV DROPDOWNS
+  
+  //INITIATE GLIGHTBOX
+  const glightbox = GLightBox({
+    selector: '.glightbox'
+  });
+  //INITIATE GLIGHTBOX
+})
+
+
