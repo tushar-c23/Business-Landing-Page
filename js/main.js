949b49bad6504195e5cf7362ece4d33fe9c04858
tusharc <tusharc.cse@gmail.com>
2022-10-09 12:19:31 +0530

    Hero animated section created

--- js/main.js
+++ js/main.js
@@ -1,35 +1,51 @@
-// MOBILE NAV TOGGLE
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
 
-const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
-if(mobileNavToggle) {
-  mobileNavToggle.addEventListener('click',function(event) {
-    event.preventDefault();
+  // STICKY HEADER ON SCROLL
 
-    document.querySelector('body').classList.toggle('mobile-nav-active');
+  const selectHeader = document.querySelector('#header');
+  if(selectHeader) {
+    document.addEventListener('scroll', () => {
+      window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
+    });
+  }
 
-    this.classList.toggle('bi-list');
-    this.classList.toggle('bi-x');
-  });
-}
+  // STICKY HEADER ON SCROLL
 
-// MOBILE NAV TOGGLE
-
-// TOGGLE MOBILE NAV DROPDOWNS
-
-const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
-
-navDropdowns.forEach(el => {
-  el.addEventListener('click', function(event) {
-    if(document.querySelector('.mobile-nav-active')) {
+  // MOBILE NAV TOGGLE
+  
+  const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
+  if(mobileNavToggle) {
+    mobileNavToggle.addEventListener('click',function(event) {
       event.preventDefault();
-      this.classList.toggle('active');
-      this.nextElementSibling.classList.toggle('dropdown-active');
+  
+      document.querySelector('body').classList.toggle('mobile-nav-active');
+  
+      this.classList.toggle('bi-list');
+      this.classList.toggle('bi-x');
+    });
+  }
+  
+  // MOBILE NAV TOGGLE
+  
+  // TOGGLE MOBILE NAV DROPDOWNS
+  
+  const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+  
+  navDropdowns.forEach(el => {
+    el.addEventListener('click', function(event) {
+      if(document.querySelector('.mobile-nav-active')) {
+        event.preventDefault();
+        this.classList.toggle('active');
+        this.nextElementSibling.classList.toggle('dropdown-active');
+  
+        let dropDrownIndicator = this.querySelector('.dropdown-indicator');
+        dropDrownIndicator.classList.toggle('bi-chevron-up');
+        dropDrownIndicator.classList.toggle('bi-chevron-down');
+      }
+    })
+  });
+  
+  // TOGGLE MOBILE NAV DROPDOWNS
+})
 
-      let dropDrownIndicator = this.querySelector('.dropdown-indicator');
-      dropDrownIndicator.classList.toggle('bi-chevron-up');
-      dropDrownIndicator.classList.toggle('bi-chevron-down');
-    }
-  })
-});
-
-// TOGGLE MOBILE NAV DROPDOWNS
